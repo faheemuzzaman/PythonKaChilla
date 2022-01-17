485343ae529a7733e3cf0a3bdb6393211821570a
Sheikh M. Sahal <s.m.sahal786@outlook.com>
2022-01-17 12:57:22 +0500

    some data missed

--- Day13/app.py
+++ Day13/app.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import xlsxwriter
 import sys
 import csv
 from re import search
@@ -13,19 +14,30 @@ arrCity = df1['City']
 arrCheck = df2['values']
 arrCode = df1['Code']
 
-f=open('final.csv','w+')
 
 
+
+
+workbook = xlsxwriter.Workbook('Final.xlsx')
+ 
+# The workbook object is then used to add new
+# worksheet via the add_worksheet() method.
+worksheet = workbook.add_worksheet()
+row = 0
+column = 0
+ 
+# Use the worksheet object to write
+# data via the write() method.
 for x in range(len(arrCity)):
-    # print(arrCity[x])
-    for y in range(len(arrCheck)):
-        # if arrCity[x] in arrCheck[y]:
-        if arrCity[x].find(arrCheck[y]) != -1:
-        # print(x,y)
-        # if search(arrCode[x],arrCheck[y]):
-            f.write(arrCode[x]+","+arrCity[y]+"\n")
-f.close()
-# df1.sort_values(sys.argv[3])
-# df2.sort_values(sys.argv[3])
-#df1.drop(df1.columns[list(map(int, sys.argv[4].split()))], axis = 1, inplace = True)
-#df2.drop(df2.columns[list(map(int, sys.argv[4].split()))], axis = 1, inplace = True)
+    # print(arrCity[x]+","+arrCode[x])
+       for y in range(len(arrCheck)):
+            if arrCity[x].strip() in arrCheck[y].strip():
+             print(arrCheck[y].strip()+","+arrCode[x].strip())
+             worksheet.write(row,column, arrCheck[y].strip())
+             worksheet.write(row,column+1, arrCode[x].strip())
+             row +=1
+             break
+             
+# Finally, close the Excel file
+# via the close() method.
+workbook.close()
